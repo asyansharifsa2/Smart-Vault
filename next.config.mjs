a661d558d6ad385cf84d957e6874801659338a8b
asyansharifsa2 <asyansharifsa2@gmail.com>
2026-07-09 23:06:12 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // 👈 Tambahkan baris ini untuk ekspor statis ke GitHub Pages
   typescript: {
     ignoreBuildErrors: true,
   },
